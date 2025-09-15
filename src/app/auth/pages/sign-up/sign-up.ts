6b6dfa6ce3eabc384b076fb7efd10c847f7f221b
Juan Pablo <jpcardonab0520@gmail.com>
2025-09-14 19:57:52 -0500

    feat(auth) sign-up and login pages with standard routes

--- src/app/auth/pages/sign-up/sign-up.ts
+++ src/app/auth/pages/sign-up/sign-up.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { AuthHeader } from '../../components/auth-header/auth-header';
+import { RegisterCard } from '../../components/register-card/register-card';
+import { WelcomeMessage } from '../../components/welcome-message/welcome-message';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [RegisterCard, WelcomeMessage, AuthHeader],
   templateUrl: './sign-up.html',
-  styleUrl: './sign-up.css'
+  styleUrl: './sign-up.css',
 })
-export class SignUp {
-
-}
+export class SignUp {}
