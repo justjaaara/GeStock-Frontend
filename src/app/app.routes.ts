fcc56faf1e9f1e070fef412e9c3032dac34ca49b
Juan Pablo <jpcardonab0520@gmail.com>
2025-09-14 20:01:14 -0500

    feat(auth): sign-up and login page routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/pages/login/login';
+import { SignUp } from './auth/pages/sign-up/sign-up';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'sign-up', component: SignUp },
+];
