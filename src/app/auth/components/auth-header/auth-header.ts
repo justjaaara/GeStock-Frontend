624ac4b4b6ad9e85033ffb4443596e0c5fab8dbc
Juan Pablo <jpcardonab0520@gmail.com>
2025-09-14 19:54:19 -0500

    feat(auth): add auth-header component

---
+++ src/app/auth/components/auth-header/auth-header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-header',
+  imports: [],
+  templateUrl: './auth-header.html',
+  styleUrl: './auth-header.css'
+})
+export class AuthHeader {
+
+}
