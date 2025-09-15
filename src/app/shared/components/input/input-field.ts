a717b59db39b731a113cb5e38f9c5def4458a734
Juan Pablo <jpcardonab0520@gmail.com>
2025-09-14 19:59:11 -0500

    feat(shared) add button and input-field shared components

--- src/app/shared/components/input/input-field.ts
+++ src/app/shared/components/input/input-field.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+
 @Component({
-  selector: 'app-button',
+  selector: 'app-input-field',
   imports: [CommonModule],
-  templateUrl: './button.html',
-  styleUrl: './button.css',
+  templateUrl: './input-field.html',
+  styleUrl: './input-field.css',
 })
-export class Button {
-  @Input() text: string = '';
+export class InputField {
+  @Input() placeholder: string = '';
   @Input() textColor: string = '';
   @Input() backgroundColor: string = '';
   @Input() width: string = '';
