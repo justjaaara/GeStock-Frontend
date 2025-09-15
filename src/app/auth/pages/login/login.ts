8171f35bdbc74ccc4c6a91dba1d593e5346bebf4
Juan Pablo <jpcardonab0520@gmail.com>
2025-09-14 20:16:13 -0500

    style(auth): add styles for login page

--- src/app/auth/pages/login/login.ts
+++ src/app/auth/pages/login/login.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { AuthHeader } from '../../components/auth-header/auth-header';
+import { LoginCard } from '../../components/login-card/login-card';
+import { WelcomeMessage } from '../../components/welcome-message/welcome-message';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [LoginCard, WelcomeMessage, AuthHeader],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
 })
-export class Login {
-
-}
+export class Login {}
