33185fb5b5e5f2c85808ef6b3be535705a61b211
Juan Pablo <jpcardonab0520@gmail.com>
2025-09-14 19:56:04 -0500

    feat(auth): add welcome-message component

---
+++ src/app/auth/components/welcome-message/welcome-message.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-message',
+  imports: [],
+  templateUrl: './welcome-message.html',
+  styleUrl: './welcome-message.css'
+})
+export class WelcomeMessage {
+
+}
