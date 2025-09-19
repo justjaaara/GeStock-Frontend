0dd32f3de6789daec32c6ade8ba02749585c6784
Felipe Villa <villajaramillofelipe4@gmail.com>
2025-09-18 21:20:13 -0500

    feat(dashboard): add dashboard component with HTML, CSS, and unit tests

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './auth/pages/login/login';
 import { SignUp } from './auth/pages/sign-up/sign-up';
+import { Dashboard } from './core-ui/pages/dashboard/dashboard';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'sign-up', component: SignUp },
+  { path: 'dashboard', component: Dashboard },
 ];
