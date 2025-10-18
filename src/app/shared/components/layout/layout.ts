55fb24593fdeccfd9f28ad492f30249c03194b1c
Felipe Villa <villajaramillofelipe4@gmail.com>
2025-10-18 17:47:03 -0500

    feat(layout): add logout modal and improve layout structure

--- src/app/shared/components/layout/layout.ts
+++ src/app/shared/components/layout/layout.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterLink } from '@angular/router';
-import { Sidebar } from '../sidebar/sidebar';
+import { Component, inject, signal } from '@angular/core';
 import { Header } from '@/shared/services/header';
+import { Auth } from '@/auth/services/auth';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { Sidebar } from '@/shared/components/sidebar/sidebar';
+import { Modal } from '@/shared/components/modal/modal';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, Sidebar],
+  imports: [RouterOutlet, RouterLink, Sidebar, Modal],
   templateUrl: './layout.html',
-  styleUrl: './layout.css'
+  styleUrl: './layout.css',
 })
 export class Layout {
-  constructor(public header: Header) { }
+  header = inject(Header);
+  private authService = inject(Auth);
+
+  showLogoutModal = signal(false);
+
+  openLogoutModal(): void {
+    this.showLogoutModal.set(true);
+  }
+
+  confirmLogout(): void {
+    this.authService.logout();
+  }
+
+  cancelLogout(): void {
+    this.showLogoutModal.set(false);
+  }
 }
